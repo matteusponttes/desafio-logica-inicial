44ee67f24bc03ef686769d377f55b0e63b565546
matteusponttes <matteus.ponttes@gmail.com>
2024-02-15 00:27:28 -0300

    Adicionado começo do novo desafio Calculadora de Ranqueadas

---
+++ calculadoraRanked.js
@@ -0,0 +1,11 @@
+let vitoria = 12
+let derrota = 2
+
+
+function saldo (){
+    resultado = vitoria - derrota
+    return
+}
+
+saldo();
+console.log("O Herói tem de saldo de **" + saldo + "** está no nível de **{nivel}**")
