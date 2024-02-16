d34521260e8defbb909b90cf2cfd6c8cce72917a
matteusponttes <matteus.ponttes@gmail.com>
2024-02-15 23:18:55 -0300

    Inclusão dos Elos referente as vitórias

--- calculadoraRanked.js
+++ calculadoraRanked.js
@@ -1,6 +1,6 @@
-let vitoria = 12
+let vitoria = 35
 let derrota = 2
-
+let elo
 
 function saldo (){
     resultado = vitoria - derrota
@@ -8,4 +8,27 @@ function saldo (){
 }
 
 saldo();
-console.log("O Herói tem de saldo de **" + saldo + "** está no nível de **{nivel}**")
+
+if (resultado <= 10){
+    elo = "Ferro"
+}
+if ((resultado > 10) && (resultado <= 20)){
+    elo = "Bronze"
+}
+if ((resultado > 20) && (resultado <= 50)){
+    elo = "Prata"
+}
+if ((resultado > 50) && (resultado <= 80)){
+    elo = "Ouro"
+}
+if ((resultado > 80) && (resultado <= 90)){
+    elo = "Diamante"
+}
+if ((resultado > 90) && (resultado <= 100)){
+    elo = "Lendário"
+}
+if (resultado > 100){
+    elo = "Imortal"
+}
+
+console.log("O Herói tem de saldo de **" + resultado + "** e está no nível **" + elo + "**")
